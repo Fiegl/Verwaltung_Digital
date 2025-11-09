84351e1e9ea8e6811d244c7178bfb5e03f9e4d66
Fiegl <Stefan.Fiegl@gmx.de>
2025-11-09 19:36:01 +0100

    funktion personenstandsregister

--- einwohnermeldeamt/views.py
+++ einwohnermeldeamt/views.py
@@ -1,3 +1,30 @@
+import uuid
+import json
+import requests
+import datetime as date
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from pathlib import Path                            #funktioniert PLattform-übergreifend
 
+
+
+
+
+
+# Pfade zu den Registern
+personenstandsregister = Path("db/personenstandsregister.json")
+
+
+
+
+def lade_personenstandsregister():
+    if not personenstandsregister.exists():
+        return []
+    try:
+        with open(personenstandsregister, "r", encoding="utf-8") as file:
+            data = json.load(file)
+            if isinstance(data, dict):
+                return []
+            return data
+    except json.JSONDecodeError:
+        return []
