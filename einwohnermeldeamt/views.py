2fefcffece10add8f6cf29fce3148c74ba98fdc1
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-11-17 13:06:50 +0100

    neues update

--- einwohnermeldeamt/views.py
+++ einwohnermeldeamt/views.py
@@ -1,46 +1,84 @@
 import uuid
 import json
-import requests
 import time
 import datetime as date
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from pathlib import Path                            #funktioniert PLattform-übergreifend
-from lxml import tree
+from django.views.decorators.csrf import csrf_exempt 
 
 
 
 
 
 # Pfade zu den Registern
-personenstandsregister = Path("db/personenstandsregister.json")
+personenstandsregister = "db/personenstandsregister.json"
 
 
+
+
+def test_api(request):
+    return render(request, "einwohnermeldeamt/test_api.html")
+
+
+def test(request):
+    return render(request, "einwohnermeldeamt/test.html")
 
 
 def lade_personenstandsregister():
-    if not personenstandsregister.exists():
-        return []
     try:
         with open(personenstandsregister, "r", encoding="utf-8") as datei:
-            daten = json.load(datei)
-            if isinstance(daten, dict):
-                return []
-            return daten
-    except json.JSONDecodeError:
+            return json.load(datei)
+    except:
         return []
+
+    
+
+def speichere_personenstandsregister(daten):
+    with open (personenstandsregister, "w", encoding="utf-8") as datei:
+        json.dump(daten, datei, ensure_ascii=False, indent=2)
+        
+
     
 
 
-def erzeuge_public_key():
-    pass
-
-def erzeuge_buerger_id():
-    pass
 
 
-def personenstandsregister_api(request):                                #muss auf den Produktiv-Server
-    with open("/db/personenstandsregister.json", "r") as datei:
-        daten = json.load(datei)
-    return JsonResponse(daten)
+
+#API_URL = "http://[2001:7c0:2320:2:f816:3eff:fef8:f5b9]:8000/einwohnermeldeamt/personenstandsregister_api"
+
+
+
+@csrf_exempt
+def personenstandsregister_api(request):
+
+    if request.method == "POST":
+
+        vorname = request.POST.get("vorname")
+        nachname_geburt = request.POST.get("nachname_geburt")
+        geburtsdatum = request.POST.get("geburtsdatum")
+
+    #with open("/var/www/django-project/test.txt", "w", encoding="utf-8") as datei:
+    #datei.write(str(request.POST))
+
+        erstelle_neuen_eintrag = {
+            "buerger_id": str(uuid.uuid4()),
+            "vorname": vorname,                     #schickt uns Gesumdheit&Soziales
+            "nachname_geburt": nachname_geburt,     #schickt uns Gesumdheit&Soziales
+            "geburtsdatum": geburtsdatum,           #schickt uns Gesumdheit&Soziales
+            "sterbedatum": None,
+            "familienstand": "ledig",
+            "haft_status": None,
+            "steuer_id": None,
+        }
+
+        daten = lade_personenstandsregister()
+        daten.append(erstelle_neuen_eintrag)
+        speichere_personenstandsregister(daten)
+
+        
+        return HttpResponse("Daten gespeichert")
+
+    
+    return HttpResponse("Nur POST erlaubt", status=405)
+
 
