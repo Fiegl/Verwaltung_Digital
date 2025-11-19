e2e5317fd4612ab8cdb5cc99941cd9f61a189e84
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-11-19 09:07:06 +0100

    update

--- einwohnermeldeamt/urls.py
+++ einwohnermeldeamt/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("personenstandsregister_api", views.personenstandsregister_api, name="personenstandsregister_api"), #Slash bei API's weglassen
-    path("wohnsbuerger_service/", views.wohnbuerger_service, name="wohnsbuerger_service"),
+    path("wohnsbuerger_services/", views.buerger_services, name="buerger_services"),
     path("abfrage_buerger_id", views.abfrage_buerger_id, name="abfrage_buerger_id"),
     path('test/', views.test, name='test'),
     path('test_api/', views.test_api, name="test_api"),
