0cf22d100efdd4498e8f508bded957d20bd8de5f
Fiegl <167548499+Fiegl@users.noreply.github.com>
2025-12-01 18:00:12 +0100

    neue datei e-sigatur + neue json dokumente

---
+++ einwohnermeldeamt/e-signatur.py
@@ -0,0 +1,19 @@
+from lxml import etree
+from signxml import XMLSigner, XMLVerifier
+
+data_to_sign = """<Test heute="2024-12-11">:-)</Test>"""
+
+cert = open("cert.pem").read()
+key = open("privkey.pem").read()
+
+root = etree.fromstring(data_to_sign)
+
+signed_root = XMLSigner().sign(root, cert=cert, key=key)
+signed_root
+with open("signed.xml", "wb") as datei:
+    datei.write(etree.tostring(signed_root))
+#verified_data = XMLVerfifier().verify(signed__root).signed_xml
+
+with open("signed.xml") as signierteDatei:
+    signed = etree.parse(signierteDatei)
+XMLVerifier().verify(signed, x509_cert=cert).signed_xml
